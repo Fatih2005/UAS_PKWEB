db3112ac4fab539aadad5d19786cdf13f3eb3f8f
Flokin <flokinrayyanalfatih@gmail.com>
2026-06-26 13:49:27 +0700

    fix: user biasa tidak bisa ubah status atau assigned_to saat edit ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -113,12 +113,18 @@ class TicketController extends Controller
     public function update(TicketUpdateRequest $request, $ticket)
     {
         $ticket = Ticket::findOrFail($ticket);
+        $user = Auth::user();
 
-        if (! $ticket->canBeManagedBy(Auth::user())) {
+        if (! $ticket->canBeManagedBy($user)) {
             abort(403);
         }
 
         $data = $request->validated();
+
+        if (! $user->is_admin) {
+            $data['status'] = $ticket->status;
+            $data['assigned_to'] = $ticket->assigned_to;
+        }
 
         if ($request->hasFile('attachment')) {
             $file = $request->file('attachment');
